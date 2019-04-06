979a418540ec3f247e8013d58efe1d9752c44694
Nikita <39918253+Giro5@users.noreply.github.com>
2019-04-06 15:02:57 +0500

    branch

---
+++ public/branch/custom_br.js
@@ -0,0 +1,63 @@
+// //
+// $(".test1").ready(function () {
+//     console.dir(123);
+// });
+// //styles
+// $(".test1").css("color", "red");
+
+// $(".test1").css({
+//     "box-shadow": "black 5px 0 4px",
+//     "background": "green",
+//     "fontSize": "30px",
+//     "font-weight": "bolder"
+// });
+// //data
+// //attribute "value"
+// console.log($(".test2").val());
+// //parie tags
+// console.log($(".test1").text());
+// console.log($(".test1").html());
+// //write data
+// $(".test2").val("mytext<hr>");
+// $(".test1").text("mytext<hr>");
+// $(".test1").html("mytext<hr>");
+
+// console.log($(".test2").val());
+// console.log($(".test1").text());
+// console.log($(".test1").html());
+
+// //adding attributes
+// $("p").addClass("newP");
+// //$("p").attr("class", "newP");
+// $(".newP").attr("width", 100);
+// $(".newP").text("new tag P");
+
+// //removing attributes
+// $("p").removeAttr("class");
+
+// //events jquery
+// //1 attribute
+// //2  events js
+// //mouse
+// //drag'n'drop the element
+// //loading file
+// //moves mouse
+// //input from keyboard
+// $("#uniqueBtn").on({
+//     click: function () {
+//         alert("hello");
+//     },
+//     dblclick: function () {
+//         alert("twice hello");
+//     },
+//     contextmenu: function () {
+//         alert("context hello");
+//     }
+// });
+
+//animation
+$(".animate").animate({
+    //css
+}, {
+        //options animate
+    })
